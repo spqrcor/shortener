82379894e510ffa96887232f963b075ae7acee7d
Пешков Дмитрий <d.peshkov@etpgpb.ru>
2024-07-03 22:23:37 +0300

    iter3 init

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
 	"net/http"
 )
 
 var store = map[string]string{}
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, createShortHandler(store))
-	mux.HandleFunc(`/{id}`, searchShortHandler(store))
-
-	err := http.ListenAndServe(`:8080`, mux)
-	if err != nil {
+	r := chi.NewRouter()
+	r.Post("/", createShortHandler(store))
+	r.Get("/{id}", searchShortHandler(store))
+	r.HandleFunc(`/*`, func(res http.ResponseWriter, req *http.Request) {
+		res.WriteHeader(http.StatusBadRequest)
+	})
+	err := http.ListenAndServe(":8080", r)
+	if err == nil {
 		panic(err)
 	}
 }
