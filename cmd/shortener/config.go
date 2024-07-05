cb525572ca90c30f0fdf23146db791498bd262ec
Пешков Дмитрий <d.peshkov@etpgpb.ru>
2024-07-05 14:47:21 +0300

    iter5 init

--- cmd/shortener/config.go
+++ cmd/shortener/config.go
@@ -1,12 +1,36 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"github.com/caarlos0/env/v6"
+	"log"
+)
 
 var flagRunAddr string
 var flagBaseURL string
 
+type Config struct {
+	addr    string `env:"SERVER_ADDRESS"`
+	baseURL string `env:"BASE_URL"`
+}
+
 func parseFlags() {
-	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
-	flag.StringVar(&flagBaseURL, "b", "http://localhost:8080", "base url")
+	var cfg Config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cfg.addr != "" {
+		flagRunAddr = cfg.addr
+	} else {
+		flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
+	}
+
+	if cfg.baseURL != "" {
+		flagBaseURL = cfg.baseURL
+	} else {
+		flag.StringVar(&flagBaseURL, "b", "http://localhost:8080", "base url")
+	}
 	flag.Parse()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module shortener
 go 1.22
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-chi/chi/v5 v5.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
