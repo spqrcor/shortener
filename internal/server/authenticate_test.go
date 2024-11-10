91736e60eaecd5f045591597682c006f9bfec586
Пешков Дмитрий <d.peshkov@etpgpb.ru>
2024-11-10 14:12:20 +0300

    iter22 add tests

---
+++ internal/server/authenticate_test.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"net/http"
+	"net/http/httptest"
+	"shortener/internal/authenticate"
+	"shortener/internal/config"
+	"shortener/internal/handlers"
+	"shortener/internal/logger"
+	"shortener/internal/storage"
+	"testing"
+)
+
+func Test_authenticateMiddleware(t *testing.T) {
+	loggerRes, _ := logger.NewLogger(zap.InfoLevel)
+	conf := config.NewConfig()
+	store := storage.CreateMemoryStorage(conf)
+	authService := authenticate.NewAuthenticateService(
+		authenticate.WithLogger(loggerRes),
+		authenticate.WithSecretKey(conf.SecretKey),
+		authenticate.WithTokenExp(conf.TokenExp),
+	)
+	r := chi.NewRouter()
+	r.Use(authenticateMiddleware(loggerRes, authService))
+	r.Post(`/`, handlers.CreateShortHandler(store))
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+	t.Run("work_auth", func(t *testing.T) {
+		r := httptest.NewRequest("POST", srv.URL+"/", nil)
+		r.RequestURI = ""
+		r.Header.Set("Content-Type", "text/html")
+		resp, _ := http.DefaultClient.Do(r)
+		require.Equal(t, http.StatusBadRequest, resp.StatusCode)
+		defer func() {
+			err := resp.Body.Close()
+			require.NoError(t, err)
+		}()
+	})
+}
